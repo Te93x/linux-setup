436b1d4fcc5e11ec8e825740d0d5fcb096c24321
Te <52430464+Te93x@users.noreply.github.com>
2026-06-11 14:00:37 +0800

    Install additional libraries in github-runner-setup.sh
    Added additional packages required for the setup.

--- github-runner-setup.sh
+++ github-runner-setup.sh
@@ -2,3 +2,15 @@ set -euo pipefail
 
 sudo apt update
 sudo apt install -y libatomic1
+
+sudo apt-get install \
+    libnspr4 \
+    libnss3 \
+    libatk1.0-0t64 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxfixes3 \
+    libxrandr2 \
+    libgbm1 \
+    libasound2t64 \
+    libatspi2.0-0t64
